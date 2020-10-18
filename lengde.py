a1f114547814a4b1640b6598b5f5cda03f87bb68
annabettina <72708591+annabettina@users.noreply.github.com>
2020-10-18 13:30:28 +0200

    Anna

---
+++ lengde.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def lengder(knutepunkt, element, nelem):
+ 
+    elementlengder = np.zeros((nelem, 1))
+    # Beregner elementlengder med Pythagoras' læresetning
+    for i in range (0, nelem):
+        # OBS! Grunnet indekseringsyntaks i Python-arrays vil ikke denne funksjonen fungere naar vi bare har ett element.
+        dx = knutepunkt[element[i, 0], 0] - knutepunkt[element[i, 1], 0]
+        dy = knutepunkt[element[i, 0], 1] - knutepunkt[element[i, 1], 1]
+        elementlengder[i] = np.sqrt(dx*dx + dy*dy)
+ 
+    return elementlengder
