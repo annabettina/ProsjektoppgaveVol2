e98d666712730771004db347c1f8325229711020
adgund <60650998+adgund@users.noreply.github.com>
2020-10-19 17:22:12 +0200

    Update lastvektor.py

--- lastvektor.py
+++ lastvektor.py
@@ -1,31 +1,16 @@
 import numpy as np
 
-def lastvektor(nelem, last , fastinnspeningsmoment):
-    
-    #Lastvektoren inneholder to bidrag
-    #1)Konsentrerte knutepunktskrefter og momenter i knutepunktene
-    #2)Knutepunktskrefter og momenter som skyldes last i felt
-    
-    #Lager en array der man kan legge inn lastene
-    #nelem er lengden på arrayet
-    #tallet en er hvor mange delemententer hvert element betsår av
-    lastvektor = np.zeros(nelem, 1)
-    
-    
-    #Legger inn momenter som skyldes krefter i felt,
-    #dvs fastinnspeningsmoment
-    
-    #for i in fastinnspeningsmoment:
-        
-    #Legger til ytre krefter som vikrer i knutepunktene
-    #Det er kun en av lastene som bidrar til dette
-    #og det er momentet i element nr 7
-    
-    for m in last:
-        if m[0]==3:#sjekker om lasten er et moment
-            knutepunktmoment = m[8] #heter ut lastvektoren
-            lastventor[m+1]=knutepunktmoment #legger til momentet i lastvektoren
-    print(lastvektor)
+def lastvektor ( npunkt , nelem , elem , fim , nlast , last ):
+   
+    lastvektor = np.zeros ((npunkt , 1))
+    for i in range (nelem):
+        knpkt1 = elem[i][0]
+        knpkt2 = elem[i][1]
+        lastvektor[knpkt1-1] -= fim[i][0]
+        lastvektor[knpkt2-1] -= fim[i][1] 
+
+        for j in range (nlast):
+            knpunktLast = int(last[j][7])
+            lastvektor[knpunktLast-1] += last[j][8]
+
     return lastvektor
-    
-    lastvektor()
